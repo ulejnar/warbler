21cf6056811082e294bdaa7b2ddfae5f687b75b0
Graham Trail <Graham@Grahams-MacBook-Pro.local>
2020-04-02 16:12:55 -0700

    More user tests.

--- test_user_model.py
+++ test_user_model.py
@@ -9,6 +9,8 @@ import os
 from unittest import TestCase
 from flask_bcrypt import Bcrypt
 from models import db, User, Message, Follows
+from sqlalchemy import exc
+
 bcrypt = Bcrypt()
 
 # BEFORE we import our app, let's set an environmental variable
@@ -41,8 +43,8 @@ class UserModelTestCase(TestCase):
         Follows.query.delete()
 
         #Test hashing?
-        USER1_PWD = bcrypt.generate_password_hash("test").decode('UTF-8')
-        USER2_PWD = bcrypt.generate_password_hash("secret").decode('UTF-8')
+        # USER1_PWD = bcrypt.generate_password_hash("test").decode('UTF-8')
+        # USER2_PWD = bcrypt.generate_password_hash("secret").decode('UTF-8')
 
         test_user1 = User(username="TEST_USER1", 
                           email="TEST1@EMAIL.COM", 
@@ -50,7 +52,7 @@ class UserModelTestCase(TestCase):
                           header_image_url="TEST1_HEADER_IMAGE",
                           bio="TEST1 BIO",
                           location="TEST1 LOCATION",
-                          password=USER1_PWD)
+                          password="test")
         
         test_user2 = User(username="TEST_USER2", 
                           email="TEST2@EMAIL.COM", 
@@ -58,7 +60,7 @@ class UserModelTestCase(TestCase):
                           header_image_url="TEST2_HEADER_IMAGE",
                           bio="TEST2 BIO",
                           location="TEST2 LOCATION",
-                          password=USER2_PWD)
+                          password="secret")
 
         db.session.add(test_user1)
         db.session.add(test_user2)
@@ -66,17 +68,12 @@ class UserModelTestCase(TestCase):
 
         self.client = app.test_client()
 
+    def tearDown(Self):
+        # super teardown?
+        db.session.rollback()
+
     def test_user_model(self):
         """Does basic model work?"""
-
-        # u = User(
-        #     email="test@test.com",
-        #     username="testuser",
-        #     password="HASHED_PASSWORD"
-        # )
-
-        # db.session.add(u)
-        # db.session.commit()
 
         u = User.query.filter(User.username == "TEST_USER1").first()
         u_id = u.id
@@ -122,27 +119,68 @@ class UserModelTestCase(TestCase):
 
         user1 = User.query.filter(User.username == "TEST_USER1").first()
 
+        user1.password = bcrypt.generate_password_hash("test").decode('UTF-8')
+
         self.assertEqual(User.authenticate(user1.username, "test"), user1)
         self.assertEqual(User.authenticate(user1.username, "xxxx"), False)
         self.assertEqual(User.authenticate("userX", "test"), False)
 
-    def test_signup(self):
-        """does sign up successfully crates a new user given valid credentials"""
+    def test_signup_positive(self):
+        """does signup() successfully create a new user with valid credentials?"""
 
         user_new = User.signup("BHill", "bhill@gmail.com", "password", "www.xsd.com")
-        # db.session.add(user_new)
+
         db.session.commit()
 
-        print("user object print:", User.query.filter(User.username == "BHill").first())
-        
+        #print("user object print:", User.query.filter(User.username == "BHill").first())
+
+        test_user = User.query.filter(User.username == "BHill").first() 
+
+        # Test that new user with proper username appears in DB
+        self.assertEqual(test_user.username, "BHill")
+
+        # Test that new user has properly encoded password
+        self.assertEqual(bcrypt.check_password_hash(test_user.password, "password"), True)
+
 
         # def signup(cls, username, email, password, image_url)
 
 
+    def test_signup_negative(self):
+        """does signup() fail to create a new user with invalid credentials?"""
 
+        user_new = User.signup("TEST_USER1", "bhill@gmail.com", "password", "www.xsd.com")
 
+        # Test that user with non-unique username is rejected  
+        self.assertRaises(exc.IntegrityError, db.session.commit)
+
+        db.session.rollback()
+
+        user_new = User.signup(None, "bhill@gmail.com", "password", "www.xsd.com")
+
+        # Test that user with no username is rejected  
+        self.assertRaises(exc.IntegrityError, db.session.commit)
     
+        db.session.rollback()
 
+        user_new = User.signup("BHill", "TEST1@EMAIL.COM", "password", "www.xsd.com")
+
+        # Test that user with non-unique email is rejected
+        self.assertRaises(exc.IntegrityError, db.session.commit)
+
+        db.session.rollback()
+
+        user_new = User.signup("BHill", None, "password", "www.xsd.com")
+        
+        # Test that user with no email is rejected
+        self.assertRaises(exc.IntegrityError, db.session.commit)
+        
+        #user_new = User.signup("BHill", "bhill@gmail.com", None, "www.xsd.com")
+        
+        # Test that user with no password is rejected
+        with self.assertRaises(ValueError):
+            User.signup("BHill", "bhill@gmail.com", None, "www.xsd.com")
+            
 
 
 
